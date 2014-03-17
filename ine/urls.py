6d7a5c58f53575bed1e9b051da2ef1bbcb98519d
Yasyf Mohamedali <yasyf@yasyf.com>
2014-03-16 23:41:37 -0400

    updated requirements

--- ine/urls.py
+++ ine/urls.py
@@ -3,9 +3,9 @@ from django.conf.urls import patterns, url
 from ine import views
 
 urlpatterns = patterns('',
+	url(r'^ine/resumes/all/', views.ine_resumes, name='ine_resumes'),
 	url(r'^ine/resumes/(?P<year>\d{4})/', views.ine_resumes_by_year, name='ine_resumes_by_year'),
 	url(r'^ine/resumes/(?P<industry>\d{1})/', views.ine_resumes_by_industry, name='ine_resumes_by_industry'),
-	url(r'^ine/resumes/all/', views.ine_resumes, name='ine_resumes'),
 	url(r'^ine/admin/', views.ine_admin, name='ine_admin'),
     url(r'^ine/drop/', views.ine_drop, name='ine_drop'),
     url(r'^book/(?P<_type>\d{1})/(?P<year>\d{4})/$', views.book, name='book'),
--- sbc/settings.py
+++ sbc/settings.py
@@ -27,7 +27,11 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['mitsbc.mit.edu','sbc.scripts.mit.edu','mit-sbc.org','localhost','127.0.0.1']
+#ALLOWED_HOSTS = ['mitsbc.mit.edu','http://mitsbc.herokuapp.com/','sbc.scripts.mit.edu','mit-sbc.org','localhost','127.0.0.1']
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+ALLOWED_HOSTS = ['*']
+
+
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -61,22 +65,14 @@ MEDIA_ROOT = PROJECT_PATH + '/media/'
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = PROJECT_PATH + '/static/'
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
-# Additional locations of static files
 STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(BASE_DIR, 'static'),
 )
+
 
 # List of finder classes that know how to find static files in
 # various locations.
@@ -117,7 +113,7 @@ ROOT_URLCONF = 'sbc.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'sbc.wsgi.application'
 TEMPLATE_DIRS = (
-    PROJECT_PATH + '/templates/'
+    PROJECT_PATH + '/templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -135,7 +131,7 @@ INSTALLED_APPS = (
     'south',
     'gunicorn',
     'home',
-    'ine'
+    'ine',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
