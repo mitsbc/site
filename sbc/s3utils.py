c82687da2ee96445e3fed2e29dce1f76d100a884
Yasyf Mohamedali <yasyf@yasyf.com>
2014-03-17 21:55:20 -0400

    fix admin static

--- sbc/s3utils.py
+++ sbc/s3utils.py
@@ -1,4 +1,14 @@
 from storages.backends.s3boto import S3BotoStorage
 
-StaticRootS3BotoStorage = lambda: S3BotoStorage(location='static')
-MediaRootS3BotoStorage  = lambda: S3BotoStorage(location='media')
+class CustomS3BotoStorage(S3BotoStorage):
+    def __init__(self, *args, **kwargs):
+        super(CustomS3BotoStorage, self).__init__(*args, **kwargs)
+
+    def url(self, name):
+        url = super(CustomS3BotoStorage, self).url(name)
+        if name.endswith('/') and not url.endswith('/'):
+            url += '/'
+        return url
+
+StaticRootS3BotoStorage = lambda: CustomS3BotoStorage(location='static')
+MediaRootS3BotoStorage  = lambda: CustomS3BotoStorage(location='media')
